863793ae9ae81a7c7372a037980d057977750e96
bulychenkov <kbulychenkov@mwdstd.com>
2024-11-22 11:58:08 -0600

    rop model data set

---
+++ dataset/rop_model.py
@@ -0,0 +1,32 @@
+from numpy import pi
+from dataset.units import units
+
+task = {
+    "units": units,
+    "bit": {
+        "diameter": 0.220,  # m, bit diameter
+        "cutterCount": 50,  # total cutter counts
+        "cutterRadius": 6.5e-3,  # m, cutter radii
+        "sideRakeAngle": 30 * pi / 180,  # rad, alpha angle
+        "backRakeAngle": 30 * pi / 180,  # rad, teta angle
+        "cutterWear": 2e-4,  # m, height of cutters' wear
+        "cutterWearCoefficient": 2e-19,  # m3 / N / m
+        "frictionFactor": 0.2,  # unitless, friction factor between the cutter and formation
+        "ropCorrectionCoefficient": 1  # ROP model calibration coefficient, specific for bit model
+    },
+    "formation": {
+        "abrasiveness": 10,  # unitless, [1..30]
+        "brinellHardness": 290e6,  # Pa, HBW
+        "uniaxialCompressiveStrength": 70e6  # Pa, UCS
+
+    },
+    "motor": {
+        "diffPressureOffset": 2.4e6,  # Pa
+        "speedToFlow": 90,  # rev/m3
+        "torqueToPressure": 750  # Pa/N/m
+    },
+    "surfaceRpm": 60,
+    "weightOnBit": 150000,  # N
+    "flowRate": 0.02,  # m3/s
+    "drilledDistance": 100  # m
+}
