96bb4926ea6d02361ffac1f0affd6fa9e3912021
bulychenkov <kbulychenkov@mwdstd.com>
2024-12-06 11:56:28 -0600

    changed rop task

--- dataset/rop_model.py
+++ dataset/rop_model.py
@@ -9,7 +9,7 @@ task = {
         "cutterDiameter": 13e-3,  # m, cutter diameter
         "sideRakeAngle": 30,  # deg, alpha angle
         "backRakeAngle": 30,  # deg, teta angle
-        "cutterWear": 2e-4,  # m, height of cutters' wear
+        "cutterWear": 4e-4,  # m, height of cutters' wear
         "cutterWearFactor": 2e-19,  # m3 / N / m
         "frictionFactor": 0.2,  # unitless, friction factor between the cutter and formation
         "ropCorrectionCoefficient": 1  # ROP model calibration coefficient, specific for bit model
@@ -25,8 +25,22 @@ task = {
         "revolutionsPerFlowVolume": 90,  # rev/m3
         "torqueToPressure": 750  # Pa/N/m
     },
-    "surfaceRpm": 60,
-    "weightOnBit": 15,  # t
+    "surfaceRpm": 40,
+    "weightOnBit": 14,  # t
     "flowRate": 0.02,  # m3/s
-    "drilledDistance": 100  # m
+    "drilledDistance": 24.3  # m
 }
+
+rop_ref = [
+    {'md':2024.3, 'rop':33},
+    {'md':2048.6, 'rop':33},
+    {'md':2072.9, 'rop':30},
+    {'md':2097.2, 'rop':27},
+    {'md':2121.5, 'rop':27},
+    {'md':2145.8, 'rop':37},
+    {'md':2170.1, 'rop':24},
+    {'md':2194.4, 'rop':21},
+    {'md':2218.7, 'rop':24},
+    {'md':2243, 'rop':22},
+    {'md':2267.3, 'rop':21},
+    {'md':2291.6, 'rop':22}]
--- rop.py
+++ rop.py
@@ -1,11 +1,27 @@
+import matplotlib.pyplot as plt
 import models.api as api
-from dataset.rop_model import task
+from dataset.rop_model import task, rop_ref
 
 
 def rop(task):
-    response = api.post('rop_model', task)
-    print(response)
+    modeled_rop = []
+    measured_rop = [elt['rop'] for elt in rop_ref]
+    md = [elt['md'] for elt in rop_ref]
+    for _ in range(len(md)):
+        response = api.post('rop_model', task)
+        # print(response)
+        task['bit']['cutterWear'] = response['updatedBitWear']
+        modeled_rop += [response['rop']]
+    fig, ax = plt.subplots()
+    ax.plot(md, modeled_rop, 'r-', label='Modeled ROP')
+    ax.plot(md, measured_rop, 'go-', label='Measured ROP')
+    ax.set_ylabel('ROP, m/hrs')
+    ax.set_ylim(0, 40)
+    ax.set_xlabel('MD, m')
+    plt.title('Modeled ROP with Bit Wear')
+    ax.legend()
 
 
 if __name__ == '__main__':
     rop(task)
+    plt.show()
